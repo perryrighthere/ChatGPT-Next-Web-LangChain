0620a5bd0b09b61c11d0902b0d4ae7647b7463df
perryhe <hepy.shu@gmail.com>
2024-04-30 10:57:01 +0800

    Modify auth information.

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -51,7 +51,7 @@ export function AuthPage() {
           );
         }}
       />
-      {!accessStore.hideUserApiKey ? (
+      {/* {!accessStore.hideUserApiKey ? (
         <>
           <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
           <input
@@ -77,7 +77,7 @@ export function AuthPage() {
             }}
           />
         </>
-      ) : null}
+      ) : null} */}
 
       <div className={styles["auth-actions"]}>
         <IconButton
